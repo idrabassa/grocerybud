efc7cbb23e6bd226c1782602f6ecc6c71f531e28
Israel Rabassa <idrabassa@gmail.com>
2021-06-20 18:46:31 -0400

    Updates

---
+++ precache-manifest.00ec6ff2216b4b60d4b5c344398afa83.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc81feed30e75442a283374d15177cc7",
+    "url": "/grocerybud/index.html"
+  },
+  {
+    "revision": "dc8a0bb0b02f576a4fcf",
+    "url": "/grocerybud/static/css/main.0d747718.chunk.css"
+  },
+  {
+    "revision": "f86b35d4e5ed4a99a4db",
+    "url": "/grocerybud/static/js/2.22b6d6b3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/grocerybud/static/js/2.22b6d6b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc8a0bb0b02f576a4fcf",
+    "url": "/grocerybud/static/js/main.838cb3cf.chunk.js"
+  },
+  {
+    "revision": "6f584da86c929975cdfc",
+    "url": "/grocerybud/static/js/runtime-main.9095b050.js"
+  }
+]);
